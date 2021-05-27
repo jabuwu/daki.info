c3a309f577c8933e28b9ac8856cdc219b74b701a
jabu <jaburu@dakimakuri.com>
2021-05-27 21:02:15 +0000

    add survey route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+app.get('/survey', (_req, res) => {
+  res.redirect('https://docs.google.com/spreadsheets/d/1yvjI3EQmvhkN4q5PBlJwjyeLbLpNC-QIpVWq5Xn7cB8/edit?usp=sharing');
+});
 app.use((_req, res) => {
   res.redirect('https://docs.google.com/document/d/1oIlLt1uqutTP8725wezfZ2mjc-IPfOFCdc6hlRIb-KM/edit');
 });
